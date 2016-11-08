4e24446e439b2b7c40b9305c535ebae29932286f
EricCheng鄭穎懋(資訊) <ericcheng@taiwankk.com>
2016-11-08 11:09:19 +0800

    init

---
+++ src/main/java/onlytest/Pet2016Application.java
@@ -0,0 +1,12 @@
+package onlytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pet2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pet2016Application.class, args);
+	}
+}
