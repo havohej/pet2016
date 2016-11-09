5ab9e8fb821c0c4e7df71fdd0648e7a9c174cdf4
havohej <havohej@gmail.com>
2016-11-09 23:27:43 +0800

    update h2 db setting & mybatis

--- src/main/java/onlytest/Pet2016Application.java
+++ src/main/java/onlytest/Pet2016Application.java
@@ -1,7 +1,10 @@
 package onlytest;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Pet2016Application {
@@ -9,4 +12,12 @@ public class Pet2016Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Pet2016Application.class, args);
 	}
+
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
+	}
+
 }
---
+++ src/main/java/onlytest/dao/ITestDAO.java
@@ -0,0 +1,15 @@
+package onlytest.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface ITestDAO {
+
+	@Select("select * from test")
+	List<Map<String, Object>> select();
+
+}
